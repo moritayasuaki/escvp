979f6763c56a546b75a6795bcc5f6daba3d6e034
Morita Yasuaki <zousandazou@gmail.com>
2012-08-29 08:02:03 +0900

    kernel 3.2

--- escvp.c
+++ escvp.c
@@ -26,9 +26,7 @@
 #define VENDOR_ID 0x04b8
 #define PRODUCT_ID  0x0514
 
-static bool debug;
-
-static int escvp_startup(struct usb_serial *serial);
+static int debug;
 
 static const struct usb_device_id id_table[] = {
   { USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
@@ -37,42 +35,47 @@ static const struct usb_device_id id_table[] = {
 
 MODULE_DEVICE_TABLE(usb, id_table);
 
+static struct usb_driver escvp_driver = {
+  .name = "escvp",
+  .probe = usb_serial_probe,
+  .disconnect = usb_serial_disconnect,
+  .id_table = id_table,
+  .no_dynamic_id = 1,
+};
+
 static struct usb_serial_driver escvp_device = {
   .driver = {
     .owner = THIS_MODULE,
     .name = "escvp",
   },
   .id_table = id_table,
+  .usb_driver = &escvp_driver,
   .num_ports = 1,
-  .attach = escvp_startup,
 };
-
-static struct usb_serial_driver * const serial_drivers[] = {
-  &escvp_device, NULL
-};
-
-static int escvp_startup(struct usb_serial *serial)
-{
-  if (serial->num_bulk_out == 0){
-    printk(KERN_INFO "ESC/VP21 VCOM: missing bulk out endpoint\n");
-    return -EINVAL;
-  }
-  printk(KERN_INFO "ESC/VP21 VCOM: usb-serial attached\n");
-  return 0;
-}
 
 static int __init escvp_init(void)
 {
-  return usb_serial_register(&escvp_device);
+  int retval;
+  retval = usb_serial_register(&escvp_device);
+  if (retval)
+    return retval;
+  retval = usb_register(&escvp_driver);
+  if (retval)
+    usb_serial_deregister(&escvp_device);
+  return retval;
 }
 
 static void __exit escvp_exit(void)
 {
+  usb_deregister(&escvp_driver);
   usb_serial_deregister(&escvp_device);
 }
 
 module_init(escvp_init);
 module_exit(escvp_exit);
+
+module_param(debug, bool, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(debug, "Debug enabled or not");
 
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
