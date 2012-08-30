5ad5e80637ae64780d8e4ce228e9016a7e540e7e
Morita Yasuaki <zousandazou@gmail.com>
2012-08-31 00:14:48 +0900

    add timer

--- escvp.c
+++ escvp.c
@@ -15,6 +15,7 @@
 #include <linux/usb.h>
 #include <linux/usb/serial.h>
 #include <linux/kfifo.h>
+#include <linux/timer.h>
 
 /* drvier's version */
 #define DRIVER_VERSION   "v0.1"
@@ -27,6 +28,65 @@
 #define PRODUCT_ID  0x0514
 
 static int debug;
+
+struct escvp_private {
+  struct timer_list timer;
+  struct urb *control_urb;
+};
+
+static void escvp_polling(unsigned long arg){
+  struct tty_struct *tty = (struct tty_struct *)arg;
+  struct usb_serial_port *port = tty->driver_data;
+  struct escvp_private *priv;
+
+  priv = usb_get_serial_port_data(port);
+  printk(KERN_INFO "test\n");
+  priv->timer.expires = jiffies + 1*HZ;
+  add_timer(&(priv->timer));
+}
+
+static int escvp_open(struct tty_struct *tty, struct usb_serial_port *port)
+{
+  int retval;
+  struct escvp_private *priv;
+  priv = usb_get_serial_port_data(port);
+  priv->timer.expires = jiffies + 1*HZ;
+  priv->timer.function = escvp_polling;
+  priv->timer.data = (unsigned long)(tty);
+  retval = usb_serial_generic_open(tty, port);
+  if (retval)
+    return retval;
+  add_timer(&(priv->timer));
+  return 0;
+}
+
+static void escvp_close(struct usb_serial_port *port)
+{
+  struct escvp_private *priv;
+  priv = usb_get_serial_port_data(port);
+  usb_serial_generic_close(port);
+  del_timer_sync(&(priv->timer));
+  usb_serial_generic_close(port);
+}
+
+static int escvp_attach(struct usb_serial *serial)
+{
+  struct escvp_private *priv;
+  priv = kmalloc(sizeof(struct escvp_private), GFP_KERNEL);
+  if (!priv)
+    return -ENOMEM;
+  init_timer(&(priv->timer));
+  usb_set_serial_port_data(serial->port[0], priv);
+  return 0;
+}
+
+static void escvp_release(struct usb_serial *serial)
+{
+  struct escvp_private *priv;
+  priv = usb_get_serial_port_data(serial->port[0]);
+  del_timer_sync(&(priv->timer));
+  kfree(priv);
+}
 
 static const struct usb_device_id id_table[] = {
   { USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
@@ -51,6 +111,10 @@ static struct usb_serial_driver escvp_device = {
   .id_table = id_table,
   .usb_driver = &escvp_driver,
   .num_ports = 1,
+  .attach = escvp_attach,
+  .release = escvp_release,
+  .open = escvp_open,
+  .close = escvp_close,
 };
 
 static int __init escvp_init(void)
